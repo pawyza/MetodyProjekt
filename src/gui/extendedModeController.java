2f6d0ba845f1676db3a73702567e2b5563f7f17b
David <236807@student.pwr.edu.pl>
2019-01-09 21:06:31 +0100

    Dodane Widoki

---
+++ src/gui/extendedModeController.java
@@ -0,0 +1,17 @@
+package gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class extendedModeController {
+
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private Button btnPchnijDraga;
+
+}
+
+
--- src/gui/startMenuController.java
+++ src/gui/startMenuController.java
@@ -27,17 +27,35 @@ public class startMenuController {
 
     @FXML
     void onExpandedClick(ActionEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("extendedMode.fxml"));
+            Main.stage.setScene(new Scene(root));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
+
+
 
     @FXML
     void onScoreClick(ActionEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("scoreMenu.fxml"));
+            Main.stage.setScene(new Scene(root));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
+
 
     @FXML
     void onSettingsClick(ActionEvent event) {
-
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("settingsMenu.fxml"));
+            Main.stage.setScene(new Scene(root));
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
     }
 
 }
