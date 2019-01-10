7283163c4d8d72e8b161fe559d8e76154d134389
pawyza <pawyza.f@gmail.com>
2019-01-10 15:44:20 +0100

    Dodanie funkcji do mapy.

--- src/GameUI/ClassicGameMode/ClassicGameManager.java
+++ src/GameUI/ClassicGameMode/ClassicGameManager.java
@@ -1,9 +1,11 @@
 package GameUI.ClassicGameMode;
 
+import Calculator.Integrator;
 import Exceptions.OutOfFuelException;
 import Exceptions.RocketCrashedException;
 import GameUI.GameManager;
 import Interfaces.Observer;
+import Model.Rocket;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -14,6 +16,8 @@ public class ClassicGameManager extends GameManager implements Observer {
     private Pane mapDrawingPane;
     private double mapPaneWidth;
     private double mapPaneHeight;
+    private double totalHeigt;
+    private Integrator integrator;
 
     //map background
     private Color bColor = Color.valueOf("#5A6784");
@@ -32,32 +36,66 @@ public class ClassicGameManager extends GameManager implements Observer {
     Rectangle mapRocket;
 
 
-    public ClassicGameManager(Pane gameDrawingPane,Pane mapDrawingPane) {
+    public ClassicGameManager(Pane gameDrawingPane, Pane mapDrawingPane, double totalHeight, Integrator integrator) {
         super(gameDrawingPane);
         this.mapDrawingPane = mapDrawingPane;
         mapPaneHeight = mapDrawingPane.getPrefHeight();
         mapPaneWidth = mapDrawingPane.getPrefWidth();
         mapGroundWidth = mapPaneWidth;
-        mapRocketHorizontalPosition = (mapPaneWidth/2)-(mapRocketSize/2);
+        mapRocketHorizontalPosition = (mapPaneWidth / 2) - (mapRocketSize / 2);
         mapRocketVerticalPosition = 30;
+        this.totalHeigt = totalHeight;
+        this.integrator = integrator;
         setUpMapPane();
     }
 
-    private void setUpMapPane(){
+    private void setUpMapPane() {
         System.out.println(mapRocketHorizontalPosition);
-        mapGround = new Rectangle(mapGroundWidth, mapGroundHeight,gColor);
+        mapGround = new Rectangle(mapGroundWidth, mapGroundHeight, gColor);
         mapGround.setX(0);
-        mapGround.setY(mapPaneHeight-mapGroundHeight);
+        mapGround.setY(mapPaneHeight - mapGroundHeight);
 
-        mapRocket = new Rectangle(mapRocketSize, mapRocketSize,rColor);
+        mapRocket = new Rectangle(mapRocketSize, mapRocketSize, rColor);
         mapRocket.setX(mapRocketHorizontalPosition);
         mapRocket.setY(mapRocketVerticalPosition);
-        mapDrawingPane.getChildren().addAll(mapGround,mapRocket);
+        mapDrawingPane.getChildren().addAll(mapGround, mapRocket);
+
+        Rocket rocket = Integrator.getRocket();
+
+        meterToPixelRatio = totalHeigt / (mapRocketVerticalPosition + mapRocketSize);
+        System.out.println(meterToPixelRatio);
     }
 
+    private double meterToPixelRatio;
+
+    private int blinkingTime = 0;
+    private int timeToVanish = 15;
+    private int timeToBlink = 30;
+    private boolean visible = true;
 
     @Override
     public void update() {
-        mapRocket.setFill(bColor);
+        blink();
+        moveMap();
+    }
+
+    private void moveMap() {
+        System.out.println(integrator.getRocket().getyPosition());
+    }
+
+    private void blink() {
+
+        if (!visible && blinkingTime == timeToBlink) {
+            mapRocket.setFill(rColor);
+            visible = true;
+        } else if (visible && blinkingTime == timeToVanish) {
+            mapRocket.setFill(bColor);
+            visible = false;
+        }
+        blinkingTime++;
+        if (blinkingTime > timeToBlink)
+            blinkingTime = 0;
+
+
     }
 }
